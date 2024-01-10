78dde3caca28c94618a4a93b6297a2f4a946f9cc
hacker4516 <kenan.tas@elev.ga.ntig.se>
2024-01-10 11:04:00 +0100

    test test

--- src/accountinloggning.java
+++ src/accountinloggning.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class accountinloggning {
-
+    public static void main (String [] args){
+        Scanner fortnite = new Scanner(System.in);
+        System.out.println("Fortnite proffs");
+    }
 }
